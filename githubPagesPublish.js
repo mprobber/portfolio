9dd600a698d4f42129728699174789bf4ff7a291
Michael Probber <michael@probber.com>
2018-10-23 18:57:25 -0700

    starter publish scripts, a bit messy

---
+++ githubPagesPublish.js
@@ -0,0 +1,15 @@
+const { execSync } = require('child_process');
+const BranchName = require('branch-name');
+
+async function runPublish() {
+  execSync('yarn run build:static');
+  const branchName = await BranchName.get();
+  execSync('git checkout gh-pages');
+  // execSync('git rm *')
+  execSync('git add .');
+  execSync('git commit -m "Automated publish"');
+  execSync(`git checkout ${branchName}`);
+}
+
+// eslint-disable-next-line
+runPublish().catch(console.error);
