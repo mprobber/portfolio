a8369446365398415b1e397ff78a282b58647ccb
Michael Probber <michael@probber.com>
2018-10-23 19:58:31 -0700

    Publish pretty fly

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,6 @@ const { resolve } = require('path');
 module.exports = build({
   entry: resolve(__dirname, 'index.js'),
   template: resolve(__dirname, 'index.html'),
+  favicon: resolve(__dirname, 'assets', 'favicon.ico'),
   name: 'portfolio',
 });
